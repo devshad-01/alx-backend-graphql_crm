85b35cc36edc897e6e88bb77fa4e6fdba2933ccc
Student <student@alx.com>
2025-07-13 22:16:58 +0300

    Add cron job and script to send order reminders via GraphQL

---
+++ crm/cron_jobs/send_order_reminders.py
@@ -0,0 +1,37 @@
+import requests
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
+from datetime import datetime
+
+# GraphQL endpoint
+GRAPHQL_ENDPOINT = "http://localhost:8000/graphql"
+
+# Define the GraphQL query
+QUERY = gql("""
+query {
+  allOrders(filter: {orderDateGte: """ + (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d') + """}) {
+    edges {
+      node {
+        id
+        customer {
+          email
+        }
+      }
+    }
+  }
+}
+""")
+
+# Set up the transport and client
+transport = RequestsHTTPTransport(url=GRAPHQL_ENDPOINT)
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+# Execute the query
+response = client.execute(QUERY)
+
+# Log the results
+with open("/tmp/order_reminders_log.txt", "a") as log_file:
+    for order in response['allOrders']['edges']:
+        log_file.write(f"{datetime.now()} - Order ID: {order['node']['id']}, Customer Email: {order['node']['customer']['email']}\n")
+
+print("Order reminders processed!")
