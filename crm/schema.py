a7247528b471d63435af156dae52bd3b92904492
Student <student@alx.com>
2025-07-13 22:30:49 +0300

    Add cron jobs for heartbeat logging and stock updates

--- crm/schema.py
+++ crm/schema.py
@@ -125,11 +125,30 @@ class CreateOrder(graphene.Mutation):
         order.products.set(products)
         return CreateOrder(order=order, message="Order created successfully.")
 
+class UpdateLowStockProducts(graphene.Mutation):
+    class Arguments:
+        pass
+
+    products = graphene.List(ProductType)
+    message = graphene.String()
+
+    def mutate(self, info):
+        low_stock_products = Product.objects.filter(stock__lt=10)
+        updated_products = []
+
+        for product in low_stock_products:
+            product.stock += 10
+            product.save()
+            updated_products.append(product)
+
+        return UpdateLowStockProducts(products=updated_products, message="Low stock products updated successfully.")
+
 class Mutation(graphene.ObjectType):
     create_customer = CreateCustomer.Field()
     bulk_create_customers = BulkCreateCustomers.Field()
     create_product = CreateProduct.Field()
     create_order = CreateOrder.Field()
+    update_low_stock_products = UpdateLowStockProducts.Field()
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
